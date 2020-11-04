0b7eb299bdee698756d54cfe449a8baae1b8fbfa
Moses Kamara <kamara.moses@yahoo.com>
2020-11-04 10:35:01 -0800

    started the service-worker file

--- public/js/service-worker.js
+++ public/js/service-worker.js
@@ -1,4 +1,13 @@
 // choose files to be cached
 const FILE_TO_CACHED = [
-
+    '/',
+    '/index.html',
+    '/assets/css/styles.css',
+    '/assets/js/db.js',
+    '/assets/js/index.js',
+    '/manifest.json',
+    "/assets/icons/icon_192x192.png",
+    "/assets/icons/icon_512x512.png",
+    'https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css',
+    'https://cdn.jsdelivr.net/npm/chart.js@2.8.0',
 ]
